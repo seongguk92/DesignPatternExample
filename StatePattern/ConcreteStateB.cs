7646dc62a4423e293be30ad65c80250adc3446a1
seongguk <live8681@naver.com>
2021-06-25 00:13:00 +0900

    #02 StatePattern 작업

---
+++ StatePattern/ConcreteStateB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatePattern
+{
+    class ConcreteStateB : State
+    {
+        public override void Handle1()
+        {
+            Console.WriteLine("ConcreteStateB handle1 요청");
+        }
+
+        public override void Handle2()
+        {
+            Console.WriteLine("ConcreteStateB handle2 요청");
+            Console.WriteLine("Context A상태로 변경");
+            this._context.TransitionTo(new ConcreteStateA());
+        }
+    }
+}
---
+++ StatePattern/Context.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatePattern
+{
+    class Context
+    {
+        private State _state;
+
+        public State State
+        {
+            set
+            {
+                _state = value;
+            }
+        }
+
+        public Context(State state)
+        {
+            this.TransitionTo(state);
+        }
+
+        /// <summary>
+        /// State 개체 변경
+        /// </summary>
+        public void TransitionTo(State state)
+        {
+            Console.WriteLine($"Context : Transition to {state.GetType().Name}");
+            _state = state;
+            _state.Context = this;
+        }
+
+        /// <summary>
+        /// Context 동작의 일부를 현재 상태에 위임
+        /// </summary>
+        public void Request1()
+        {
+            this._state.Handle1();
+        }
+
+        public void Request2()
+        {
+            this._state.Handle2();
+        }
+    }
+}
