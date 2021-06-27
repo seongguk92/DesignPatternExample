2b82456ba660d0380512367e47f0f5ef112fd957
seongguk <live8681@naver.com>
2021-06-28 00:50:17 +0900

    #04 AdapterPattern Add

---
+++ AdapterPattern/Adapter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdapterPattern
+{
+    public class Adapter : ITransport
+    {
+        private Car _car => new Car();
+        public void Commute()
+        {
+            _car.GasPedal();
+        }
+    }
+}
---
+++ AdapterPattern/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdapterPattern
+{
+    public class Car
+    {
+        public void GasPedal()
+        {
+            Console.WriteLine("엑셀 밟기");
+        }
+    }
+}
