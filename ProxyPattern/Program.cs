91270143a8ea4f6cf76aa857a0fc1e907923ad27
seongguk <live8681@naver.com>
2021-06-28 15:57:49 +0900

    #06 Proxy 1차 예제 작업

---
+++ ProxyPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProxyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Proxy를 호출하여 Process 실행
+            Proxy proxy = new Proxy();
+            proxy.Process();
+        }
+    }
+}
---
+++ ProxyPattern/Proxy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProxyPattern
+{
+    class Proxy : IService
+    {
+        private IService service1;
+        public void Process()
+        {
+            Console.WriteLine("Proxy는 호출에 대한 흐름 제어가 주목적!");
+
+            service1 = new Service();
+            service1.Process();
+        }
+    }
+}
