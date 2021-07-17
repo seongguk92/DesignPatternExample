ad67063e3cef0a571ef94b3a10937a0435120531
seongguk <live8681@naver.com>
2021-07-17 12:53:44 +0900

    #17 Mediator add

---
+++ MediatorPattern/DataDownloader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MediatorPattern
+{
+    public class GalleryView : ModeListener
+    {
+        public void OnModeChange(CEnum.Mode mode)
+        {
+            string showMent = mode.Equals(CEnum.Mode.LIST) ? "리스트" : "갤러리";
+            Console.WriteLine($"{showMent}뷰용 데이터 다운로드");
+        }
+    }
+}
---
+++ MediatorPattern/GalleryView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MediatorPattern
+{
+    public class DataDownloader : ModeListener
+    {
+        public void OnModeChange(CEnum.Mode mode)
+        {
+            string showMent = mode.Equals(CEnum.Mode.GALLERY) ? "Show" : "None";
+            Console.WriteLine($"갤러리 뷰 {showMent}");
+        }
+    }
+}
